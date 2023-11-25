3f3019e66dc0ee15eb2d1288e9e525b0b179fbeb
Kevin Loh <khaikhoon.loh@hotmail.com>
2023-11-26 00:39:49 +0800

    feat: Digging is hard

--- Diggable.gd
+++ Diggable.gd
@@ -1,21 +1,25 @@
 extends Area2D
 
-# var PlayerType = preload("res://Player.gd")
-
-var player_in_area = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	connect("area_entered", Callable(self, "_on_area_entered"))
+	connect("area_exited", Callable(self, "_on_area_exited"))
 	pass # Replace with function body.
 
-func _on_area_entered(_area):
-	print("entered")
-	#if area.get_script() == PlayerType:
-	#	print("in it")
-		
-func _on_area_exited(_area):
-	pass
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	pass
+
+func _on_area_entered(area):
+	if area is Player: # if it's an Area2D of type Player
+		area.dig_action.connect(Callable(self, "handle_dig"), CONNECT_ONE_SHOT)
+		print("connected!")
+
+func _on_area_exited(area):
+	if area is Player:
+		area.dig_action.disconnect(Callable(self, "handle_dig"))
+		# print("disconnected!")
+
+func handle_dig(player):
+	print("Digging!!!")
 	pass
