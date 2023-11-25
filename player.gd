3e9793617b637b2f3f802056cb121cfd5bb77576
Kevin Loh <khaikhoon.loh@hotmail.com>
2023-11-25 16:22:28 +0800

    feat: cooldown when pressing space

--- player.gd
+++ player.gd
@@ -7,6 +7,11 @@ var screen_size # Size of the game window.
 func _ready():
 	screen_size = get_viewport_rect().size
 
+func _input(InputEvent):
+	# listen to SPACEBAR (for weapon)
+	if Input.is_action_just_pressed("ui_accept"):
+		use_weapon()
+
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
@@ -29,7 +34,7 @@ func _process(delta):
 			x_direction = 1;
 			$AnimatedSprite2D.flip_h = false
 		velocity.y -= 1
-	
+
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -41,7 +46,37 @@ func _process(delta):
 		velocity.x - velocity.y,
 		(velocity.x + velocity.y) / 2
 	)
-	
+
 #	position += velocity * delta
 	position += isometric_velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+	# update cooldown timer & cooldown bar
+	if cooldown_timer > 0:
+		cooldown_timer -= delta
+		$Cooldown.value = (weapon_cooldown_duration - cooldown_timer) / weapon_cooldown_duration * 100
+
+		if cooldown_timer <= 0:
+			on_cooldown_finished()
+
+
+
+var weapon_cooldown_duration = 3.0
+var cooldown_timer = 0.0
+
+func use_weapon():
+	if cooldown_timer <= 0:
+		# TODO: Use weapon, do things to its UI etc
+		start_cooldown()
+	else:
+		# TODO: Play weapon disabled sound
+		print("Weapon is on cooldown")
+
+func start_cooldown():
+	cooldown_timer = weapon_cooldown_duration
+	$Cooldown.show()
+	$Cooldown.value = 0.0
+
+func on_cooldown_finished():
+	$Cooldown.hide()
+	cooldown_timer = 0.0
