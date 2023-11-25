37031ae722191ede81c22913937681cbeda57ec2
Bunyod16 <bunyodshams@gmail.com>
2023-11-25 19:24:31 +0800

    feat: player animation

--- player.gd
+++ player.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
 var x_direction = 1
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+var current_animation = null
+var animation : AnimationPlayer
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	animation = $Sprites/AnimationPlayer
+
+	# Play the idle animation when the scene starts
+	animation.play("IdleRight")
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
@@ -13,30 +20,31 @@ func _process(delta):
 		velocity.x += 1
 		if (x_direction != 1):
 			x_direction = 1;
-			$AnimatedSprite2D.flip_h = false
+			$Sprites.scale = Vector2(1, 1);
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 		if (x_direction != -1):
 			x_direction = -1;
-			$AnimatedSprite2D.flip_h = true
+			$Sprites.scale = Vector2(-1, 1);
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 		if (x_direction != -1):
 			x_direction = -1;
-			$AnimatedSprite2D.flip_h = true
+			$Sprites.scale = Vector2(-1,1)
 	if Input.is_action_pressed("move_up"):
 		if (x_direction != 1):
 			x_direction = 1;
-			$AnimatedSprite2D.flip_h = false
+			$Sprites.scale = Vector2(1,1)
 		velocity.y -= 1
 	
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.animation = "walking"
+		animation.play("WalkingRight")
+		#$AnimatedSprite2D.animation = "walking"
 	else:
-		$AnimatedSprite2D.animation = "idle"
-	$AnimatedSprite2D.play()
+		animation.play("IdleRight")
+	#$AnimatedSprite2D.play()
 	var isometric_velocity = Vector2(
 		velocity.x - velocity.y,
 		(velocity.x + velocity.y) / 2
