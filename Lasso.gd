3609e1be61f668719a4917cd816643787b269507
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2023-11-27 12:01:33 +0800

    gold math logic

--- Lasso.gd
+++ Lasso.gd
@@ -29,12 +29,24 @@ func _on_area_2d_area_entered(area):
 	
 	var shooter_obj = GameManager.Players[shooter_id]
 	var receiver_obj = GameManager.Players[receiver_id]
+	
+	
+	var can_steal = 3 - shooter_obj.collected_gold
+	var gold_stolen = min(receiver_obj.collected_gold, can_steal)
+	
+	print("can_steal: ", can_steal)
+	print("gold_stolen: ", gold_stolen)
+	
+	if gold_stolen < 0:
+		return
+	if receiver_obj.collected_gold == 3 or can_steal == 0:
+		return
 
 	print("before", GameManager.Players)
 	
 	#Shooter of lasso gets all his gold
-	GameManager.update_player_information(shooter_id, shooter_obj.name, shooter_obj.health, shooter_obj.gold, shooter_obj.collected_gold + receiver_obj.collected_gold)
+	GameManager.update_player_information(shooter_id, shooter_obj.name, shooter_obj.health, shooter_obj.gold, shooter_obj.collected_gold + gold_stolen)
 	
-	#Receiver of lasso loses all gold_collected
-	GameManager.update_player_information(receiver_id, receiver_obj.name, receiver_obj.health, receiver_obj.gold ,0)
+	#Receiver of lasso loses all collected_gold
+	GameManager.update_player_information(receiver_id, receiver_obj.name, receiver_obj.health, receiver_obj.gold, receiver_obj.collected_gold - gold_stolen)
 	print("Lasso HIT!") # Replace with function body.
